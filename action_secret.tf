a671c8d88f5381741f99d820f70dcf2a93f6ef84
Joseph Marhee <joseph.marhee@suse.com>
2023-12-06 16:19:01 -0600

    Adds comment to action_secret.tf for a TO DO
    Signed-off-by: Joseph Marhee <joseph.marhee@suse.com>

--- action_secret.tf
+++ action_secret.tf
@@ -1,6 +1,7 @@
 resource "github_actions_secret" "example_secret" {
-  depends_on      = [rancher2_token.service_token]
-  repository      = var.github_repo_name
-  secret_name     = var.github_secret_name
+  depends_on  = [rancher2_token.service_token]
+  repository  = var.github_repo_name
+  secret_name = var.github_secret_name
+  # This should be an encrypted_value, but for POC purposes:
   plaintext_value = nonsensitive(rancher2_token.service_token.token)
 }
